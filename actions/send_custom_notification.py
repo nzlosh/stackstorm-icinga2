cb8302a0a963318e3c9359829b8a1ff894894790
Carlos <nzlosh@yahoo.com>
2020-03-02 08:00:15 +0100

    Implemented delay notifications and send custom notification

--- actions/send_custom_notification.py
+++ actions/send_custom_notification.py
@@ -1,8 +1,21 @@
 from lib.icinga2 import Icinga2Action
 
 
-class Icinga2GetHost(Icinga2Action):
-    def run(self, host=""):
-        if host == "":
-            return self._client.objects.list("Host")
-        return self._client.objects.list("Host", name=host)
+class Icinga2SendCustomNotification(Icinga2Action):
+    def run(
+        self,
+        object_type,
+        filters,
+        author,
+        comment,
+        filter_vars=None,
+        force=False
+    ):
+        return self._client.actions.send_custom_notification(
+            object_type,
+            filters,
+            author,
+            comment,
+            filter_vars,
+            force
+        )
